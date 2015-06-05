d3e60f32d8377ba8b58d4eda1dad71c10b8da9a8
johannesprinz <johannesprinz@gmail.com>
2015-06-05 12:33:14 +1200

    First Kata Session progress

---
+++ codekata/KarateChop/Class1.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+
+namespace KarateChop
+{
+    [TestFixture]
+    public class Class1
+    {
+        const int unsuccessful = -1;
+        const int empty = 0;
+        private Karate SUT;
+
+        [SetUp]
+        public void SetUp()
+        {
+            SUT = new Karate();
+        }
+
+        [Test]
+        public void Given_Null_Array_Returns_Unsuccessful()
+        {
+            var result = SUT.chop(It.IsAny<int>(), null);
+            result.Should().Be(unsuccessful);
+        }
+        [Test]
+        public void Given_Empty_Array_Returns_Unsuccessful()
+        {
+            var result = SUT.chop(It.IsAny<int>(), new int[empty]);
+            result.Should().Be(unsuccessful);
+        }
+        [Test]
+        public void Given_Integer_Not_in_Array_Returns_Unsuccessful()
+        {
+            var result = SUT.chop(1, new[] {2} );
+            result.Should().Be(unsuccessful);
+        }
+        [Test]
+        public void Given_Integer_in_Array_Returns_Index()
+        {
+            var result = SUT.chop(1, new[] { 1 });
+            result.Should().Be(0);
+        }
+    }
+}
---
+++ codekata/KarateChop/Karate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KarateChop
+{
+    internal class Karate
+    {
+        const int unsuccessful = -1;
+        public Karate()
+        {
+        }
+
+        internal int chop(int searchValue, int[] intArray)
+        {
+            if (intArray == null) return unsuccessful;
+            if (intArray.Length > 0 )
+            {
+                return 0;
+            }
+      
+            return unsuccessful;
+        }
+    }
+}
